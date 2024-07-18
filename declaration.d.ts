5045ac86619dcb08caf0db8f7191806084efba29
dhiren-mhatre <kp064669@gmail.com>
2024-07-18 23:59:13 +0530

    Fix Tailwind CSS installation and configuration

--- declaration.d.ts
+++ declaration.d.ts
@@ -1,6 +1,4 @@
 declare module 'tailwindcss' {
-  import { PluginCreator } from 'postcss';
-  export type Config = Record<string, any>;
-  const tailwindcss: PluginCreator<Config>;
+  const tailwindcss: any;
   export default tailwindcss;
 }
